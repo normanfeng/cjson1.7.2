aa136d79611e9eafcb847531c148605327ddb5d5
冯潇洋 <89370726+normanfeng@users.noreply.github.com>
2025-05-26 10:25:12 +0000

    initial

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/sourcecode/code/cjson-1.7.2/cJSON-1.7.2
+# Build directory: /root/sourcecode/code/cjson-1.7.2/cJSON-1.7.2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(cjson_test "/root/sourcecode/code/cjson-1.7.2/cJSON-1.7.2")
+set_tests_properties(cjson_test PROPERTIES  _BACKTRACE_TRIPLES "/root/sourcecode/code/cjson-1.7.2/cJSON-1.7.2/CMakeLists.txt;236;add_test;/root/sourcecode/code/cjson-1.7.2/cJSON-1.7.2/CMakeLists.txt;0;")
